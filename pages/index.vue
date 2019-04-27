bbabd59f846c0c2f55977f82ab3c18e244aca197
David Royer <droyer01@gmail.com>
2019-04-27 10:31:40 -0400

    fix: fixes link to repo

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <h2 class="card-text">
         The code for this demo is available on Github
       </h2>
-      <b-button to="https://github.com/davidroyer/nuxtcms-demo">
+      <b-button href="https://github.com/davidroyer/nuxtcms-demo">
         NuxtCMS Demo Repo</b-button
       >
       <!-- <b-card title="Github Repo">
